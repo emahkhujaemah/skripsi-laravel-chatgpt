67f8a80609dad2790baf8f48e35bc9288ba5302a
emah khujaemah <khuza.emah24@gmail.com>
2023-06-12 11:34:22 +0700

    add model analysis sentiment

---
+++ database/migrations/2023_06_12_042948_create_labelling_data_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLabellingDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('labelling_data', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id');
+            $table->string('opinion');
+            $table->string('positif');
+            $table->string('netral');
+            $table->string('negatif');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('labelling_data');
+    }
+}
