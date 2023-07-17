a16e2836e93c5f54c038801f1aa0e06eb53ff77b
emah khujaemah <khuza.emah24@gmail.com>
2023-07-17 10:36:50 +0700

    update view landing-page

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -9,7 +9,7 @@
                 <div class="col-lg-6 align-self-center text-center text-lg-start mb-lg-5">
                     <div class="btn btn-sm border rounded-pill text-white px-3 mb-3 animated slideInRight">Politeknik Negeri Indramayu</div>
                     <h1 class="display-4 text-white mb-4 animated slideInRight">Sentiment Analysis of ChatGPT </h1>
-                    <p class="text-white mb-4 animated slideInRight">Website yang digunakan untuk analisis sentimen terhadap ChatGPT</p>
+                    <p class="text-white mb-4 animated slideInRight">Website used to predict tweets and other sentiments on ChatGPT topics</p>
                     <a href="" class="btn btn-light py-sm-3 px-sm-5 rounded-pill me-3 animated slideInRight">Read More</a>
                     <a href="" class="btn btn-outline-light py-sm-3 px-sm-5 rounded-pill animated slideInRight">Contact Us</a>
                 </div>
@@ -26,12 +26,12 @@
         <div class="container py-5">
             <div class="row g-5 align-items-center">
                 <div class="col-lg-5 wow fadeIn" data-wow-delay="0.1s">
-                    <div class="btn btn-sm border rounded-pill text-primary px-3 mb-3">Our Services</div>
-                    <h1 class="mb-4">Our Excellent AI Solutions for Your Business</h1>
-                    <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum et tempor sit. Aliqu diam
-                        amet diam et eos labore. Clita erat ipsum et lorem et sit, sed stet no labore lorem sit. Sanctus
-                        clita duo justo et tempor eirmod magna dolore erat amet</p>
-                    <a class="btn btn-primary rounded-pill px-4" href="">Read More</a>
+                    <div class="btn btn-sm border rounded-pill text-primary px-4 mb-3">Our Services</div>
+                    <h1 class="mb-4">Analysis Sentiment</h1>
+                    <p class="mb-4">Sentiment analysis is the process of analyzing digital text to determine if the emotional tone of the message is positive, negative, or neutral. Today, companies have large volumes of text data like emails, customer support chat transcripts, social media comments, and reviews.</p>
+                    <p>A sentiment analysis system helps companies improve their products and services based on genuine and specific customer feedback. AI technologies identify real-world objects or situations (called entities) that customers associate with negative sentiment. From the above example, product engineers focus on improving the processor's heat management capability because the text analysis software associated disappointed (negative) with processor (entity) and heats up (entity).</p>
+                    <p>NLP technologies further analyze the extracted keywords and give them a sentiment score. A sentiment score is a measurement scale that indicates the emotional element in the sentiment analysis system. It provides a relative perception of the emotion expressed in text for analytical purposes. For example, researchers use 10 to represent satisfaction and 0 for disappointment when analyzing customer reviews.</p>
+                    <a class="btn btn-primary rounded-pill px-4" href="https://tinyurl.com/4ssjatc7">Read More</a>
                 </div>
                 <div class="col-lg-7">
                     <div class="row g-4">
@@ -42,10 +42,9 @@
                                         <div class="service-icon btn-square">
                                             <i class="fa fa-robot fa-2x"></i>
                                         </div>
-                                        <h5 class="mb-3">Robotic Automation</h5>
-                                        <p>Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet
-                                            diam sed stet lorem.</p>
-                                        <a class="btn px-3 mt-auto mx-auto" href="">Read More</a>
+                                        <h5 class="mb-3">Natural language processing | NLP</h5>
+                                        <p>Natural language processing (NLP) is the ability of a computer program to understand human language as it is spoken and written referred to as natural language. It is a component of artificial intelligence (AI)</p>
+                                        <a class="btn px-3 mt-auto mx-auto" href="https://www.techtarget.com/searchenterpriseai/definition/natural-language-processing-NLP">Read More</a>
                                     </div>
                                 </div>
                                 <div class="col-12 wow fadeIn" data-wow-delay="0.5s">
@@ -53,9 +52,8 @@
                                         <div class="service-icon btn-square">
                                             <i class="fa fa-power-off fa-2x"></i>
                                         </div>
-                                        <h5 class="mb-3">Machine learning</h5>
-                                        <p>Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet
-                                            diam sed stet lorem.</p>
+                                        <h5 class="mb-3">Recurrent Neural Network | LSTM</h5>
+                                        <p>LSTM is better in terms of capturing the memory of sequential information better than simple RNNs. To understand the theoretical aspects of LSTM please visit the article Long Short Term Memory Networks Explanation. Due to increased complexity than that of GRU, it is slower to train but in general, LSTMs give better accuracy than GRUs.</p>
                                         <a class="btn px-3 mt-auto mx-auto" href="">Read More</a>
                                     </div>
                                 </div>
@@ -68,10 +66,9 @@
                                         <div class="service-icon btn-square">
                                             <i class="fa fa-graduation-cap fa-2x"></i>
                                         </div>
-                                        <h5 class="mb-3">Education & Science</h5>
-                                        <p>Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet
-                                            diam sed stet lorem.</p>
-                                        <a class="btn px-3 mt-auto mx-auto" href="">Read More</a>
+                                        <h5 class="mb-3">Deep learning</h5>
+                                        <p>Deep learning is a subset of machine learning, which is essentially a neural network with three or more layers. These neural networks attempt to simulate the behavior of the human brain—albeit far from matching its ability—allowing it to “learn” from large amounts of data</p>
+                                        <a class="btn px-3 mt-auto mx-auto" href="https://towardsdatascience.com/cnn-sentiment-analysis-9b1771e7cdd6">Read More</a>
                                     </div>
                                 </div>
                                 <div class="col-12 wow fadeIn" data-wow-delay="0.7s">
@@ -79,10 +76,9 @@
                                         <div class="service-icon btn-square">
                                             <i class="fa fa-brain fa-2x"></i>
                                         </div>
-                                        <h5 class="mb-3">Predictive Analysis</h5>
-                                        <p>Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet
-                                            diam sed stet lorem.</p>
-                                        <a class="btn px-3 mt-auto mx-auto" href="">Read More</a>
+                                        <h5 class="mb-3">Convolution Neural Network</h5>
+                                        <p>Convolutional neural networks, or CNNs, form the backbone of multiple modern computer vision systems. Image classification, object detection, semantic segmentation — all these tasks can be tackled by CNNs</p>
+                                        <a class="btn px-3 mt-auto mx-auto" href="https://www.geeksforgeeks.org/sentiment-analysis-with-an-recurrent-neural-networks-rnn/">Read More</a>
                                     </div>
                                 </div>
                             </div>
